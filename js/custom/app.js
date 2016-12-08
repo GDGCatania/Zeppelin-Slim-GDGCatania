a70e31e8989d40bf2f77352d3b58c3b1270cebaf
Tkd-Alex <alex.tkd.alex@gmail.com>
2016-12-08 22:17:03 +0100

    Update event tabs

---
+++ js/custom/app.js
@@ -0,0 +1,41 @@
+var myApp = angular.module('myApp', []).config(function($interpolateProvider){
+    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+});
+
+myApp.filter('html', ['$sce', function ($sce) {
+    return function (text) {
+        return $sce.trustAsHtml(urlify(text.replace(/<br\s*[\/]?>/gi, '\b\n').replace('•','')));
+    };
+}])
+
+function urlify(text) {
+    var urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.replace(urlRegex, function(url) {
+        return '<a href="' + url + '">' + url + '</a>';
+    })
+}
+
+myApp.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}]);
+
+myApp.controller('eventsController', function($scope, $http){
+
+  $scope.colorsRnd = [
+                       "#E91E63",
+                       "#3F51B5",
+                       "#FF5722",
+                       "#00BCD4",
+                       "#8BC34A",
+                       "#f44336",
+                       "#607D8B"
+                     ];
+
+  var url = "https://hub.gdgx.io/api/v1/chapters/118102220008611585984/events?page=1&perpage=6&sort=start&asc=-1";
+  $.ajax({url: url, type: "GET", dataType: "jsonp", async: false, success: function(result){
+      $scope["eventi"] = result;
+      $scope.$digest();
+  }});
+
+
+});
