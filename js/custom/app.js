34ad57be64056d5d46021e8136cc86f4ad68c6b1
pietrobiondi <pietro.biondi94@gmail.com>
2016-12-19 16:08:59 +0100

    Update partners section, split by key partners, update Federica Foto

--- js/custom/app.js
+++ js/custom/app.js
@@ -32,7 +32,7 @@ myApp.controller('eventsController', function($scope, $http){
                      ];
 
   var url = "https://hub.gdgx.io/api/v1/chapters/118102220008611585984/events?page=1&perpage=6&sort=start&asc=-1";
-  $.ajax({url: url, type: "GET", dataType: "jsonp", async: false, success: function(result){
+  $.ajax({url: url, type: "GET", dataType: "jsonp", async: true, success: function(result){
       $scope["eventi"] = result;
       $scope.$digest();
   }});
